931396c4224ed60506ba06eabdb0d61459c2aca6
tjog <28024277+tjog@users.noreply.github.com>
2024-03-27 14:32:41 +0100

    fixed file system example from internal repo
    did not work with the public version of s3k at master branch

--- common/inc/plat/qemu_virt.h
+++ common/inc/plat/qemu_virt.h
@@ -6,6 +6,8 @@
 #define UART_NS16550A
 #define UART0_BASE_ADDR (0x10000000ull)
 
+#define VIRTIO0_BASE_ADDR (0x10001000ull)
+
 #define MTIME_BASE_ADDR 0x200bff8ull
 #define MTIMECMP_BASE_ADDR 0x2004000ull
 
@@ -29,7 +31,7 @@
 	{                                                             \
 		[0] = cap_mk_pmp(0x20005fff, MEM_RWX),                \
 		[1] = cap_mk_memory(0x80020000, 0x80100000, MEM_RWX), \
-		[2] = cap_mk_memory(0x10000000, 0x10001000, MEM_RW),  \
+		[2] = cap_mk_memory(UART0_BASE_ADDR, UART0_BASE_ADDR + 0x1000, MEM_RW), \
 		[3] = cap_mk_memory(0x200b000, 0x200c000, MEM_R),     \
 		[4] = cap_mk_time(0, 0, S3K_SLOT_CNT),                \
 		[5] = cap_mk_time(1, 0, S3K_SLOT_CNT),                \
@@ -37,4 +39,5 @@
 		[7] = cap_mk_time(3, 0, S3K_SLOT_CNT),                \
 		[8] = cap_mk_monitor(0, S3K_PROC_CNT),                \
 		[9] = cap_mk_channel(0, S3K_CHAN_CNT),                \
+		[10] = cap_mk_memory(VIRTIO0_BASE_ADDR, VIRTIO0_BASE_ADDR + 0x1000, MEM_RW), \
 	}
