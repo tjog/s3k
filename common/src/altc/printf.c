dd9e0215fb84292ccde8f2933a97fda66fade6a1
tjog <28024277+tjog@users.noreply.github.com>
2024-03-29 17:05:22 +0100

    add basic decimal printing
    still missing padding etc

--- common/src/altc/printf.c
+++ common/src/altc/printf.c
@@ -2,10 +2,12 @@
 
 #include <stdarg.h>
 
+#define MAXBUF (sizeof(long int) * 8) /* enough for binary */
+
 int alt_printf(const char *fmt, ...)
 {
-	static const char hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-				     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	static const char hex[16]
+	    = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	unsigned long long x;
 	int len;
 	va_list args;
@@ -25,6 +27,31 @@ int alt_printf(const char *fmt, ...)
 			break;
 		case 's':
 			len += alt_putstr(va_arg(args, char *));
+			break;
+		case 'd':
+			char buf[MAXBUF]; /* build number here */
+			register char *p = &buf[MAXBUF - 1];
+			int sign_char = 0;
+			long r = va_arg(args, long);
+			if (r >= 0) {
+				x = r;
+				sign_char = 0; // TODO: perhaps add support for plus sign
+			} else {
+				x = -r;
+				sign_char = '-';
+			}
+			do {
+				*p-- = hex[x % 10];
+				x /= 10;
+			} while (x != 0);
+			if (sign_char) {
+				alt_putchar(sign_char);
+				len++;
+			}
+			while (++p != &buf[MAXBUF]) {
+				alt_putchar(*p);
+				len++;
+			}
 			break;
 		case 'x':
 			x = va_arg(args, unsigned int);
