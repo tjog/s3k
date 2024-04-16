b822b2df9c757ea955930d661358f3faf3f80e79
tjog <28024277+tjog@users.noreply.github.com>
2024-04-16 16:38:20 +0200

    move some string stuff into common rather than being kernel exclusive

--- common/inc/altc/string.h
+++ common/inc/altc/string.h
@@ -1,4 +1,28 @@
 #include <stddef.h>
+#include <stdint.h>
+
+// The RISC-V GNU toolchain does not seem to define ssize_t
+typedef long ssize_t;
+_Static_assert(sizeof(ssize_t) == sizeof(size_t));
+
+/**
+ * strlcat - Append a length-limited, C-string to another
+ * The strlcat() function appends the NUL-terminated
+ * string src to the end of dst. It will append at most
+ * size - strlen(dst) - 1 bytes, NUL-terminating the result.
+ * 
+ * This is not the "pure" version, as an error is returned
+ * if the src string would not fit
+ */
+ssize_t strlcat(char *dest, const char *src, size_t count);
+/**
+ * strlen - Find the length of a string
+  */
+size_t strlen(const char *s);
+/**
+ * strscpy - Copy a C-string into a sized buffer
+ */
+ssize_t strscpy(char *dest, const char *src, size_t count);
 
 void *memcpy(void *dst, const void *src, size_t n);
 void *memmove(void *dst, const void *src, size_t n);
---
+++ common/src/altc/string.c
@@ -0,0 +1,52 @@
+#include "altc/string.h"
+
+size_t strlen(const char *s)
+{
+	const char *sc;
+	for (sc = s; *sc != '\0'; ++sc)
+		/* nothing */;
+	return sc - s;
+}
+
+ssize_t strscpy(char *dest, const char *src, size_t count)
+{
+	long res = 0;
+
+	if (count == 0)
+		return -1; // Too small
+
+	while (count) {
+		char c;
+
+		c = src[res];
+		dest[res] = c;
+		if (!c)
+			return res;
+		res++;
+		count--;
+	}
+
+	/* Hit buffer length without finding a NUL; force NUL-termination. */
+	if (res)
+		dest[res - 1] = '\0';
+
+	return -1; // Too big
+}
+
+ssize_t strlcat(char *dest, const char *src, size_t count)
+{
+	size_t dsize = strlen(dest);
+	size_t len = strlen(src);
+	size_t res = dsize + len;
+
+	if (res >= count)
+		return -1; // Too big (equals because we need null terminator)
+
+	dest += dsize;
+	count -= dsize;
+	if (len >= count)
+		len = count - 1;
+	memcpy(dest, src, len);
+	dest[len] = 0;
+	return res;
+}
