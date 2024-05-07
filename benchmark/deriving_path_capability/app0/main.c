72a25ba3586cf17ef3daae6ffaaef486387bcc1a
tjog <28024277+tjog@users.noreply.github.com>
2024-05-07 13:35:50 +0200

    make assert actually fire, and use the macro like it is supposed to be
    also fix a bug where the slices were not reset between measurements, which was the bug not being detected

--- benchmark/deriving_path_capability/app0/main.c
+++ benchmark/deriving_path_capability/app0/main.c
@@ -70,14 +70,15 @@ uint64_t csrr_instret(void)
 	return instret;
 }
 
-#define ASSERT(x)                                                      \
-	{                                                              \
-		s3k_err_t err = (x);                                   \
-		if (err) {                                             \
-			alt_printf("Failed at %s: err=%d\n", #x, err); \
-			while (1)                                      \
-				;                                      \
-		}                                                      \
+#define ASSERT(x)                                                        \
+	{                                                                \
+		s3k_err_t error = (x);                                   \
+		if (error) {                                             \
+			alt_printf("Failed at %s:%d err=%d\n", __FILE__, \
+				   __LINE__, error);                     \
+			while (1)                                        \
+				;                                        \
+		}                                                        \
 	}
 
 timediff_t sample_derive()
@@ -87,36 +88,46 @@ timediff_t sample_derive()
 	uint64_t begin_instret = csrr_instret();
 	uint64_t begin_cycle = csrr_cycle();
 #if defined(SCENARIO_CAP_TY_TIME)
+	ASSERT(s3k_cap_revoke(
+	    HART3_TIME)); // the mrk of HART3_TIME needs to be reset
+	// take new start timestamps
+	begin_time = s3k_get_time();
+	begin_instret = csrr_instret();
+	begin_cycle = csrr_cycle();
 	s3k_cap_t c = s3k_mk_time(3, 0, S3K_SLOT_CNT - 1);
-	s3k_err_t err = s3k_cap_derive(HART3_TIME, S3K_CAP_CNT - 1, c);
-	ASSERT(err);
+	ASSERT(s3k_cap_derive(HART3_TIME, S3K_CAP_CNT - 1, c));
 #elif defined(SCENARIO_CAP_TY_MEM)
+	ASSERT(s3k_cap_revoke(RAM_MEM)); // the mrk of RAM_MEM needs to be reset
+	// take new start timestamps
+	begin_time = s3k_get_time();
+	begin_instret = csrr_instret();
+	begin_cycle = csrr_cycle();
 	s3k_cap_t c = s3k_mk_memory(0x80210000, 0x80220000, S3K_MEM_RWX);
-	s3k_err_t err = s3k_cap_derive(RAM_MEM, S3K_CAP_CNT - 1, c);
-	ASSERT(err);
+	ASSERT(s3k_cap_derive(RAM_MEM, S3K_CAP_CNT - 1, c));
 #elif defined(SCENARIO_CAP_TY_MEM_INTERMEDIARY)
+	ASSERT(s3k_cap_revoke(RAM_MEM)); // the mrk of RAM_MEM needs to be reset
+	// take new start timestamps
+	begin_time = s3k_get_time();
+	begin_instret = csrr_instret();
+	begin_cycle = csrr_cycle();
 	s3k_cap_t c = s3k_mk_memory(0x80210000, 0x80220000, S3K_MEM_RWX);
-	s3k_err_t err = s3k_cap_derive(RAM_MEM, S3K_CAP_CNT - 1, c);
-	ASSERT(err);
+	ASSERT(s3k_cap_derive(RAM_MEM, S3K_CAP_CNT - 1, c));
+	s3k_cap_t c2 = c;
 	for (size_t i = 0; i < 15; i++) {
-		c.mem.bgn = i;
-		c.mem.mrk = i;
-		c.mem.end = i + 1;
-		err = s3k_cap_derive(S3K_CAP_CNT - 1, S3K_CAP_CNT - 2 - i, c);
-		ASSERT(err);
+		c2.mem.bgn = c.mem.bgn + i;
+		c2.mem.mrk = c.mem.bgn + i;
+		c2.mem.end = c.mem.bgn + i + 1;
+		ASSERT(
+		    s3k_cap_derive(S3K_CAP_CNT - 1, S3K_CAP_CNT - 2 - i, c2));
 	}
 #elif defined(SCENARIO_CAP_TY_PATH)
-	s3k_err_t err = s3k_path_derive(ROOT_PATH, "a.txt", S3K_CAP_CNT - 1,
-					FILE | PATH_READ);
-	ASSERT(err);
+	ASSERT(s3k_path_derive(ROOT_PATH, "a.txt", S3K_CAP_CNT - 1,
+			       FILE | PATH_READ));
 #elif defined(SCENARIO_CAP_TY_PATH_INTERMEDIARY)
-	s3k_err_t err
-	    = s3k_path_derive(ROOT_PATH, "a", S3K_CAP_CNT - 1, PATH_READ);
-	ASSERT(err);
+	ASSERT(s3k_path_derive(ROOT_PATH, "a", S3K_CAP_CNT - 1, PATH_READ));
 	for (size_t i = 0; i < 15; i++) {
-		err = s3k_path_derive(S3K_CAP_CNT - 1, "b.txt",
-				      S3K_CAP_CNT - 2 - i, FILE | PATH_READ);
-		ASSERT(err);
+		ASSERT(s3k_path_derive(S3K_CAP_CNT - 1, "b.txt",
+				       S3K_CAP_CNT - 2 - i, FILE | PATH_READ));
 	}
 #elif defined(SCENARIO_SYS_CALL)
 	s3k_get_pid();
@@ -139,16 +150,14 @@ timediff_t sample_delete()
 	uint64_t begin_time = s3k_get_time();
 	uint64_t begin_instret = csrr_instret();
 	uint64_t begin_cycle = csrr_cycle();
-	s3k_err_t err = s3k_cap_delete(S3K_CAP_CNT - 1);
-	ASSERT(err);
+	ASSERT(s3k_cap_delete(S3K_CAP_CNT - 1));
 	uint64_t end_cycle = csrr_cycle();
 	uint64_t end_instret = csrr_instret();
 	uint64_t end_time = s3k_get_time();
 #if defined(SCENARIO_CAP_TY_PATH_INTERMEDIARY) \
     || defined(SCENARIO_CAP_TY_MEM_INTERMEDIARY)
 	for (size_t i = 0; i < 15; i++) {
-		err = s3k_cap_delete(S3K_CAP_CNT - 2 - i);
-		ASSERT(err);
+		ASSERT(s3k_cap_delete(S3K_CAP_CNT - 2 - i));
 	}
 #endif
 	t.cycle = end_cycle - begin_cycle;
@@ -163,13 +172,11 @@ timediff_t sample_revoke()
 	uint64_t begin_time = s3k_get_time();
 	uint64_t begin_instret = csrr_instret();
 	uint64_t begin_cycle = csrr_cycle();
-	s3k_err_t err = s3k_cap_revoke(S3K_CAP_CNT - 1);
-	ASSERT(err);
+	ASSERT(s3k_cap_revoke(S3K_CAP_CNT - 1));
 	uint64_t end_cycle = csrr_cycle();
 	uint64_t end_instret = csrr_instret();
 	uint64_t end_time = s3k_get_time();
-	err = s3k_cap_delete(S3K_CAP_CNT - 1);
-	ASSERT(err);
+	ASSERT(s3k_cap_delete(S3K_CAP_CNT - 1));
 	t.cycle = end_cycle - begin_cycle;
 	t.mtime = end_time - begin_time;
 	t.instret = end_instret - begin_instret;
